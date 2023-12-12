959f36be52f348f3e926eea7f858e7ab014e75cc
tomwvwest <tomwvwest@gmail.com>
2023-12-12 13:45:37 +0000

    added comments dropdown functionality

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { CommentContainer } from "./CommentContainer";
 
 export const CommentsSection = ({ comments }) => {
+  const [showComments, setShowComments] = useState(false);
+
+  const handleDropDown = () => {
+    setShowComments(!showComments);
+  };
+
   return (
     <>
-      <h3 className="comments-title">Comments</h3>
-      <div className="comments-section">
+      <h3 className="comments-title">
+        Comments{" "}
+        <img
+          src="../../images/back-arrow.png"
+          className={`comments-drop-down ${showComments? 'show-comments': 'hide-comments'}`}
+          onClick={handleDropDown}
+        />
+      </h3>
+      {showComments? <div className="comments-section">
         {comments.map((comment) => {
           return <CommentContainer comment={comment} />;
         })}
-      </div>
+      </div> : null}
     </>
   );
 };
