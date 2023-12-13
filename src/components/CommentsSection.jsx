c2f6a45572bde647a39a2d84ae577dfb119c51c1
tomwvwest <tomwvwest@gmail.com>
2023-12-13 08:58:23 +0000

    added comment vote functionality

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -12,18 +12,29 @@ export const CommentsSection = ({ comments }) => {
     <>
       <h3 className="comments-title">
         Comments ({comments.length}){" "}
-        {comments.length?<img
-          src="../../images/back-arrow.png"
-          className={`comments-drop-down ${showComments? 'show-comments': 'hide-comments'}`}
-          onClick={handleDropDown}
-        />: <p className="no-comments-message">No comments</p>}
-        
+        {comments.length ? (
+          <img
+            src="../../images/back-arrow.png"
+            className={`comments-drop-down ${
+              showComments ? "show-comments" : "hide-comments"
+            }`}
+            onClick={handleDropDown}
+          />
+        ) : (
+          <p className="no-comments-message">No comments</p>
+        )}
       </h3>
-      {showComments? <div className="comments-section">
-        {comments.map((comment) => {
-          return <CommentContainer comment={comment} />;
-        })}
-      </div> : null}
+      {showComments ? (
+        <div className="comments-section">
+          {comments.map((comment) => {
+            return (
+              <div key={comment.comment_id} className="comment-container">
+                <CommentContainer commentData={comment} />
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
     </>
   );
 };
