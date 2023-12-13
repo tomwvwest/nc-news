07ecf6a61ba65d2df0212482c652c65b0b82a14e
tomwvwest <tomwvwest@gmail.com>
2023-12-13 11:57:03 +0000

    added functionality where add comment box appears when there are no comments already

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -22,12 +22,15 @@ export const CommentsSection = ({ comments, setComments }) => {
             onClick={handleDropDown}
           />
         ) : (
-          <p className="no-comments-message">No comments</p>
+          <>
+            <p className="no-comments-message">No comments</p>
+          </>
         )}
       </h3>
+        {!comments.length ? <AddComment comments={comments} setComments={setComments} /> : null}
       {showComments ? (
         <div className="comments-section">
-          <AddComment comments={comments} setComments={setComments}/>
+          <AddComment comments={comments} setComments={setComments} />
           {comments.map((comment) => {
             return (
               <div key={comment.comment_id} className="comment-container">
